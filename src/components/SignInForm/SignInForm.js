331597b4aa55325fa80fdc1496e028bb81781576
chloeadriancreates <chloeadriancreates@gmail.com>
2023-04-25 19:06:01 +0200

    Create SignIn page

---
+++ src/components/SignInForm/SignInForm.js
@@ -0,0 +1,22 @@
+import "./SignInForm.css";
+
+export default function SignInForm() {
+    return (
+            <form>
+                <div className="input-wrapper">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" />
+                </div>
+                <div className="input-wrapper">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" />
+                </div>
+                <div className="input-remember">
+                    <input type="checkbox" id="remember-me" />
+                    <label htmlFor="remember-me">Remember me</label>
+                </div>
+                <button className="sign-in-button">Sign In</button>
+                {/* <a href="./user.html" class="sign-in-button">Sign In</a> */}
+            </form>
+    );
+}
--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,9 +1,25 @@
+import { useEffect } from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 import "./SignIn.css";
+import SignInForm from "../../components/SignInForm/SignInForm";
 
 export default function SignIn() {
+    useEffect(() => {
+        document.title = "Argent Bank – Sign in";
+    }, []);
+
     return (
-        <div>
-            Sign in
+        <div className="sign-in-page">
+            <Header />
+            <main className="main bg-dark">
+                <section className="sign-in-content">
+                    <i className="fa fa-user-circle sign-in-icon"></i>
+                    <h1>Sign In</h1>
+                    <SignInForm />
+                </section>
+            </main>
+            <Footer />
         </div>
     );
 }
