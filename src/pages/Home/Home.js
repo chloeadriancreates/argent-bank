86f35c7cb89b4862553b168363ea33f680e536f2
chloeadriancreates <chloeadriancreates@gmail.com>
2023-04-25 18:27:36 +0200

    Add Hero

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import "./Home.css";
 import Header from "../../components/Header/Header";
+import Hero from "../../components/Hero/Hero";
 
 export default function Home() {
     useEffect(() => {
@@ -10,6 +11,9 @@ export default function Home() {
     return (
         <div>
             <Header />
+            <main>
+                <Hero />
+            </main>
         </div>
     );
 }
